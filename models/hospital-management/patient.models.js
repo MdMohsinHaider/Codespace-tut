babd6e237931633c2a50d97f8bed7a54b55ab10b
Md Mohsin Haider <98874649+MdMohsinHaider@users.noreply.github.com>
2024-06-09 18:22:51 +0000

    Patient  Schema and modeling

--- models/hospital-management/patient.models.js
+++ models/hospital-management/patient.models.js
@@ -1,5 +1,42 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({},{timestamps:true})
+const patientSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        diagonsedWith:{
+            type:String,
+            required:true
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        age:{
+            type:Number,
+            required:true
+        },
+        bloodGroup:{
+            type:String,
+            enum:["A positive","A negative",
+                    "B positive","B negative",
+                    "O positive","O negative",
+                    "AB positive","AB negative"
+                ],
+            required:true
+        },
+        gender:{
+            type:String,
+            enum:["Male","Female","Other"],
+            required:true
+        },
+        admittedIn:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Hospital"
+        }
+    },{timestamps:true}
+)
 
 export const Patient = mongoose.model("Patient",patientSchema)
